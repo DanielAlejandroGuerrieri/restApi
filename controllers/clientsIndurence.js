42ebc6ae1690325b51991054590fdc6e05f34b59
Daniel Alejandro Guerrieri <daniel.guerrieri@naranjax.com>
2021-11-05 01:27:25 -0300

    feat: add endpoints for policies and clients

--- controllers/clientsIndurence.js
+++ controllers/clientsIndurence.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
 
-async function getClientsIndurance(token) {
+async function getClientsIndurance() {
     const url = `${process.env.SERVICE_URL}/clients`;
         
     try {
-        const clients = await axios.get(url,{headers: {Authorization: token}});
+        const clients = await axios.get(url,{headers: {Authorization: process.env.TOKEN_INSURANCE}});
         return clients.data;
     } catch (error) {
         throw error.response.data;
